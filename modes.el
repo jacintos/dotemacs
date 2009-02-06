00e2730451e9e6b0fc49680b6feb260033ba26cd
Jacinto Shy, Jr <jacinto.m.shy@gmail.com>
2009-02-05 23:43:48 -0500

    Added mode for Markdown

--- modes.el
+++ modes.el
@@ -5,7 +5,10 @@
 (add-to-list 'auto-mode-alist '("\\.ya?ml$" . yaml-mode))
 (autoload 'yaml-mode "yaml-mode" nil t)
 
-(add-to-list 'auto-mode-alist '("\\.cs" . csharp-mode))
+(add-to-list 'auto-mode-alist '("\\.cs$" . csharp-mode))
 (autoload 'csharp-mode "csharp-mode" nil t)
+
+(add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
+(autoload 'markdown-mode "markdown-mode.el" nil t)
 
 (ido-mode t)
